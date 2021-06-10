84afc4837dbe0c06d1d341921b81552f66fb8494
akashikaikyouohasi <hayatata@live.jp>
2021-06-11 00:04:47 +0900

    ゲーム盤を表示。なお、クリックしても動作はしない

--- volumes/react-app/src/index.js
+++ volumes/react-app/src/index.js
@@ -4,14 +4,68 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {this.props.value}  {/* props(properties)のvalueの値を表示している */}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i){
+    return <Square value={i} />;  /* Squareコンポーネントにprops(properties)としてvalueの値を渡している */
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board /> {/* Boardコンポーネントを取得している */}
+        </div>
+        <div className="geme-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+// ----------------------------------------------------
+// コンポーネントは<Game />のようにJSXの中でタグ形式で使用可能。
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Game />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
