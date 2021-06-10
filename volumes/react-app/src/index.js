9121e625587f73af4b6ab527bcacb71b4bbf9725
akashikaikyouohasi <hayatata@live.jp>
2021-06-11 00:11:43 +0900

    マス目をクリックすることで、Xが表示されるように調整。

--- volumes/react-app/src/index.js
+++ volumes/react-app/src/index.js
@@ -5,10 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
   render() {
     return (
-      <button className="square">
-        {this.props.value}  {/* props(properties)のvalueの値を表示している */}
+      <button className="square" onClick={() => this.setState({value: 'X'}) }>
+        {this.state.value}
       </button>
     );
   }
