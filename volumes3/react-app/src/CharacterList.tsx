cc2d45d52e3b11e45fd73a24dad1ca21fc944c84
akashikaikyouohasi <hayatata@live.jp>
2021-06-25 09:26:22 +0900

    途中経過

---
+++ volumes3/react-app/src/CharacterList.tsx
@@ -0,0 +1,40 @@
+import { VFC } from 'react';
+import { Header, Icon, Item } from 'semantic-ui-react'; // eslint-disable-line import/no-extraneous-dependencies
+
+export type Character = {
+    id: number;
+    name: string;
+    grade: number;
+    height?: number;
+};
+
+type Props = {
+    school: string;
+    characters: Character[];
+};
+
+const CharacterList: VFC<Props> = ({ school, characters }) => (
+  <>
+    <Header as="h2">{school}</Header>
+    <Item.Group>
+      {characters.map((character) => (
+        <Item key={character.id}>
+          <Icon name="user circle" size="huge" />
+          <Item.Content>
+            <Item.Header>{character.name}</Item.Header>
+            <Item.Meta>
+              {character.grade}
+              年生
+            </Item.Meta>
+            <Item.Meta>
+              {character.height ?? '???'}
+              cm
+            </Item.Meta>
+          </Item.Content>
+        </Item>
+      ))}
+    </Item.Group>
+  </>
+);
+
+export default CharacterList;
